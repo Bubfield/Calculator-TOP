bac4535377949af75de8b83e8592810fa4688ef3
Bubfield <69983820+Bubfield@users.noreply.github.com>
2022-06-26 18:23:41 -0500

    stuck, going to try working on other things for now

--- index.js
+++ index.js
@@ -22,14 +22,35 @@ const regButtons = document.querySelectorAll(".regButton");
 const display = document.getElementById("display");
 const equalButton = document.querySelector(".equalButton");
 const clearButton = document.querySelector(".clearButton");
+const addButton = document.querySelector(".addButton");
+const subtractButton = document.querySelector(".subtractButton");
+const multiplyButton = document.querySelector(".multiplyButton");
+const divideButton = document.querySelector(".divideButton");
 
 regButtons.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     let buttonInput = e.target.innerHTML;
     let textInput = (display.value += buttonInput);
+    //console.log(textInput);
+    addButton.addEventListener("click", function () {
+      let num = display.value;
+      display.value = "";
+      let operator = "+";
+    });
   });
 });
 
-equalButton.addEventListener("click", function () {});
+equalButton.addEventListener("click", function (num1, operator) {
+  let num = display.value;
+  if (operator === "+") {
+    operate();
+  }
+});
 
 clearButton.addEventListener("click", function () {});
+
+subtractButton.addEventListener("click", function () {});
+
+multiplyButton.addEventListener("click", function () {});
+
+divideButton.addEventListener("click", function () {});
